fd938c4387af8212d329394c8ceff14bc4f09a6e
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-09-11 22:26:02 +0300

    update: basic layout

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,39 @@
 import styles from '../styles/Home.module.css';
-import Link from 'next/link';
 import Head from 'next/head';
 import { getSession } from 'next-auth/react';
+import { motion, AnimatePresence } from 'framer-motion';
+import useBanks from '../hooks/useBanks';
 
 export default function Home({ session }) {
+  const { banks, isLoading, isError } = useBanks();
+
+  if (isError) return <h1>Something went wrong...</h1>;
+
   return (
     <>
       <Head>
         <title>Banking App</title>
         <meta name="description" content="some banks..." />
       </Head>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
+      <AnimatePresence exitBeforeEnter>
+        {isLoading ? (
+          <motion.h1
+            key={'loading'}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            Loading...
+          </motion.h1>
+        ) : (
+          <motion.div
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            key={'banks'}
+          >
+            {JSON.stringify(banks, null, 2)}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 }
