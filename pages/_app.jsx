aefc9ffca709da4b0d98213bd024ae057f5e9125
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-09-11 18:11:01 +0300

    update: added page transitions

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,27 @@
 import '../styles/globals.scss';
+import { useRouter } from 'next/router';
+import { AnimatePresence, motion } from 'framer-motion';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  return (
+    <AnimatePresence
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      exitBeforeEnter
+    >
+      <motion.div
+        key={router.route}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
+    </AnimatePresence>
+  );
 }
 
 export default MyApp;
