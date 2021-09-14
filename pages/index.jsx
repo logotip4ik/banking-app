4a75cd3cf398243039b8790f947d79ff96db7820
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-09-14 09:47:26 +0300

    update: added buttons and functions to delete and update banks

--- pages/index.jsx
+++ pages/index.jsx
@@ -83,6 +83,14 @@ export default function Home({ session }) {
     },
     [formik],
   );
+  const deleteBank = useCallback(
+    ({ id }) => {
+      fetch(`/api/bank/${id}`, { method: 'DELETE' }).then(({ ok }) =>
+        ok ? mutate('/api/bank') : null,
+      );
+    },
+    [mutate],
+  );
 
   if (isError) return <h1>Something went wrong...</h1>;
   // TODO: add mortage calculator and fetch the user banks
@@ -142,11 +150,12 @@ export default function Home({ session }) {
                       <th>Max Loan</th>
                       <th>Max Down Payment</th>
                       <th>Loan Term</th>
+                      <th>Actions</th>
                     </tr>
                   </thead>
                   <tbody className={styles.main__content__table__body}>
                     {banks.map((bank) => (
-                      <tr key={bank.name} onClick={() => editBank(bank)}>
+                      <tr key={bank.name}>
                         {Object.keys(bank)
                           .map((key) => {
                             if (key === 'id') return;
@@ -158,6 +167,34 @@ export default function Home({ session }) {
                             );
                           })
                           .filter(Boolean)}
+                        <td>
+                          <button
+                            onClick={() => editBank(bank)}
+                            className={
+                              styles.main__content__table__body__button
+                            }
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 24 24"
+                            >
+                              <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
+                            </svg>
+                          </button>
+                          <button
+                            onClick={() => deleteBank(bank)}
+                            className={
+                              styles.main__content__table__body__button
+                            }
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 24 24"
+                            >
+                              <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
+                            </svg>
+                          </button>
+                        </td>
                       </tr>
                     ))}
                   </tbody>
