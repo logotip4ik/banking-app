2be9c240b907ea6230dcff989195652ad8adf3d5
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-09-14 23:11:33 +0300

    fix: some styles + some navbar click handlers

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,22 +1,28 @@
 import styles from '../styles/Navbar.module.scss';
 import dropdownStyles from '../styles/Dropdown.module.scss';
-import { signOut } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 
-export default function Navbar({ username, profilePic }) {
+export default function Navbar() {
   const [isShowingDropdown, setIsShowingDropdown] = useState(false);
 
+  const { data: session, status } = useSession();
   const router = useRouter();
 
   return (
     <nav className={styles.nav}>
-      <h1 className={styles.nav__heading}>Hi, {username}</h1>
+      <h1
+        className={styles.nav__heading}
+        onClick={() => (router.route === '/' ? null : router.push('/'))}
+      >
+        Hi, {status !== 'loading' && session ? session.user.name : ''}
+      </h1>
 
       {/* eslint-disable-next-line */}
       <img
-        src={profilePic}
+        src={status !== 'loading' && session ? session.user.image : ''}
         alt={'User profile Picture'}
         className={styles['nav__profile-pic']}
         onClick={() => setIsShowingDropdown(!isShowingDropdown)}
@@ -33,7 +39,10 @@ export default function Navbar({ username, profilePic }) {
             <ul className={dropdownStyles.dropdown__list}>
               <li
                 className={dropdownStyles.dropdown__list__item}
-                onClick={() => router.push('/calculator')}
+                onClick={() => {
+                  router.push('/calculator');
+                  setIsShowingDropdown(false);
+                }}
               >
                 Calculate Mortgage
               </li>
