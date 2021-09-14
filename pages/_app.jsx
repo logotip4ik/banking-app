60ad25bd5e10c4701df16686a568f6d4b374666e
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-09-14 23:12:10 +0300

    update: added session provider + now navbar is global for all website

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,26 +1,31 @@
 import '../styles/globals.scss';
 import { useRouter } from 'next/router';
+import { SessionProvider } from 'next-auth/react';
 import { AnimatePresence, motion } from 'framer-motion';
+import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   return (
-    <AnimatePresence
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      exitBeforeEnter
-    >
-      <motion.div
-        key={router.route}
+    <SessionProvider session={pageProps.session}>
+      {router.route !== '/login' && <Navbar></Navbar>}
+      <AnimatePresence
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
+        exitBeforeEnter
       >
-        <Component {...pageProps} />
-      </motion.div>
-    </AnimatePresence>
+        <motion.div
+          key={router.route}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
+    </SessionProvider>
   );
 }
 
