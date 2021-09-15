6905872ecdf3d041889e246fa231fcb42b00eb04
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-09-15 21:00:14 +0300

    update: used nprogress with router events

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,27 @@
 import '../styles/globals.scss';
-import { useRouter } from 'next/router';
+import Head from 'next/head';
+import Router, { useRouter } from 'next/router';
 import { SessionProvider } from 'next-auth/react';
 import { AnimatePresence, motion } from 'framer-motion';
+import nProgress from 'nprogress';
 import Navbar from '../components/Navbar';
+
+Router.events.on('routeChangeStart', nProgress.start);
+Router.events.on('routeChangeComplete', nProgress.done);
+Router.events.on('routeChangeError', nProgress.done);
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   return (
     <SessionProvider session={pageProps.session}>
+      <Head>
+        {/* nProgress styles */}
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/nprogress@0.2.0/nprogress.css"
+        />
+      </Head>
       {router.route !== '/login' && <Navbar></Navbar>}
       <AnimatePresence
         initial={{ opacity: 0 }}
